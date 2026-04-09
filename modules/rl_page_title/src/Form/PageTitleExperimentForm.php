97690041b30dbaeef8ad997ce43c8d409f8e18d8
jur <jur@dxpr.com>
2026-04-09 09:32:32 +0200

    fix: remove unused use statement (auto-fix from pre-commit hook)

--- modules/rl_page_title/src/Form/PageTitleExperimentForm.php
+++ modules/rl_page_title/src/Form/PageTitleExperimentForm.php
@@ -7,7 +7,6 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Path\PathValidatorInterface;
 use Drupal\path_alias\AliasManagerInterface;
 use Drupal\rl\Registry\ExperimentRegistryInterface;
-use Drupal\rl_page_title\Entity\PageTitleExperiment;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
